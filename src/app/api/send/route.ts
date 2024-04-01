7deca5842d2536e34680071d460365ee397ea68d
Dang Tommy <tommy.dang@live.fr>
2024-04-01 15:46:45 +0900

    [FIX] clean code

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -5,45 +5,6 @@ import { EmailTemplate } from '../../components/email-template';
 const resend = new Resend(process.env.RESEND_API_KEY);
 const fromEmail = process.env.FROM_EMAIL;
 
-// export async function POST(req: any, res: any) {
-//   const { body } = req.json();
-//   const { email, subject, message } = body;
-
-//   try {
-//     console.log(body);
-//     const data = await resend.emails.send({
-//       from: fromEmail,
-//       to: ['tommy.pekxsvn@gmail.com', email],
-//       subject: subject,
-//       react: EmailTemplate({ subject, message}),
-//     });
-
-//     return Response.json(data);
-//   } catch (error) {
-//     return Response.json({ error });
-//   }
-// }
-
-// export async function POST() {
-//   // const { body } = req.json();
-//   // const { email, subject, message } = body;
-
-//   // console.log("BODY: ", body)
-//   try {
-//     // console.log(body);
-//     const data = await resend.emails.send({
-//       from: fromEmail,
-//       to: ['tommy.pekxsvn@gmail.com'],
-//       subject: "subject",
-//       react: EmailTemplate({ subject: "Test Subject", message: "Plop!" })
-//     });
-
-//     return Response.json(data);
-//   } catch (error) {
-//     return Response.json({ error });
-//   }
-// }
-
 interface EmailData {
   email: string;
   subject: string;
